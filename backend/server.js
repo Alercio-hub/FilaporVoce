ce2d84696adb6cc19db040efde383da220bf3b39
Alercio-hb <git@github.com:Alercio-hub/FilaConnect.git>
2025-03-15 17:07:11 +0200

    backend deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+const authRouter = require('./routes/auth');
+app.use('/api/auth', authRouter);
+
 
 // Importar rotas
 const formadoresRouter = require('./routes/formadores');
